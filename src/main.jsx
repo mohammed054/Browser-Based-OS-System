52b7ef02b42c6f8a8c0cf0cbbc654f490931808c
mohammed054 <mohammed.hassoun054@gmail.com>
2026-02-07 13:04:14 +0400

    removed dupes

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './style.css'   // ⚡ Import CSS here
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './style.css'   // ⚡ Import your CSS here
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Mount the React App
+const root = createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
 )
